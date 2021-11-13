f1fa2894eeea6db6d63232afb85aa621430de3d3
Forload <brauliovera64@gmail.com>
2021-11-13 15:41:49 -0300

    translation update

--- assets/js/translation.js
+++ assets/js/translation.js
@@ -6,14 +6,23 @@ var ThanksConsole
 let userLang = navigator.language || navigator.userLanguage
 
 if (userLang.startsWith('es')){
-    ThanksConsole = 'Gracias por usar starton'
+    ThanksConsole = 'Gracias por usar starton browser'
     greetingTimeMan = 'Buenos días'
     greetingTimeTar = 'Buenas tardes'
 }
 else {
-    ThanksConsole = ''
-    greetingTimeMan = ''
-    greetingTimeTar = ''
+    ThanksConsole = 'Thanks for use starton browser'
+    greetingTimeMan = 'Good days'
+    greetingTimeTar = 'Good afternoon'
     //
-
+    document.getElementById('close-modal1').innerHTML = 'Close'
+    /*
+    $('.lw-link-draw').innerHTML = 'like this wallpaper?'
+    $('.lbl-cor').innerHTML = 'By <a href="https://github.com/Forloaddd/" class="link-cor" translate="no">Starton</p>'
+    $('.modal-title').innerHTML = 'Settings'
+    $('#openNWI').innerHTML = 'Open in new browser tab'
+    $('#autocomplete-inp').innerHTML = 'Autocomplete'
+    $('#show-wallpaper').innerHTML = 'Show Wallpaper'
+    $('#clear-settings-btn').innerHTML = 'Restart'
+    $('#close-modal1').html('Close')*/
 }
